d0e0de101eded5767ea9058d7c7ae4059d295715
shawnstetson <78833667+shawnstetson@users.noreply.github.com>
2021-02-18 21:47:55 -0800

    Add files via upload

---
+++ Assignment01/js/script.js
@@ -0,0 +1,170 @@
+//STEP 1
+
+// let someMonth;
+// function theMonth();
+// let currentMonth;
+// let summerMonth;
+// let MyLibraryFunction
+
+//STEP 2
+
+//numeric literal expression
+//let num = prompt('Guess any number');
+//document.write (`you guessed ${num}!!!`);
+
+//string literal expression
+//let myName = prompt ("What's your name, type here");
+//document.write (`You typed your name as ${myName}`);
+
+
+//Boolean literal expression
+//let num = prompt ('5 multiplied by 5 equals?');
+//if (num == 25){alert ('Correct 25!')}
+//else {alert ('incorrect, try again')};
+
+//null literal expression.
+//let num = prompt ('5 multiplied by 5 equals?');
+//if (num == 25) {alert ('Correct 25!')}
+//else {console.log (null)};
+
+//STEP 3
+
+//let anExpression = 3 * (4 / 5) + 6;
+
+//let milesDriven = prompt('how many miles did you drive?');
+//let gallons = prompt('how many gallons of fuel does your car hold');
+//let milesPerGallon = milesDriven / gallons;
+//document.write ('your car gets ${milesPerGallon.toFixed(2)} miles per gallon.');
+
+
+//STEP 4
+
+//let firstName
+//let lastName
+//let strAddress
+//let txtCity
+//let txtState
+//let $zipCode
+//let yourAge
+//let strReferralsource
+//let txtMaywecontactyou
+
+//STEP 5
+
+//let firstName = Shawn;
+//let lastName = Stetson;
+//let textCity = San Diego;
+
+//STEP 6
+
+//let myString = Number(100);
+//document.write (`${myString}`);
+
+//let myNum = parseInt('20');
+//document.write(`${myNum}`);
+
+//STEP 7
+
+//let x = "";
+//Boolean(x); 
+//document.write (`${Boolean==x}`) 
+
+//let x = 10
+//Boolean(x)
+//document.write (`${Boolean==x}`)
+
+//STEP 8
+
+//ANSWER:FALSE, use backticks instead.
+
+//let someString = `Who once said, "Only two things are infinite, the universe and human stupidity, and I'm not sure about the former."`;
+//window.console.log(someString);
+
+//STEP 9
+
+//x = null;
+//console.log(x);
+
+//x = undefined;
+//console.log(x);
+
+//STEP 10
+
+//console.log(typeof "2");
+//console.log(typeof 2);
+//console.log(typeof 1>1);
+//console.log(typeof [2]);
+//console.log(typeof False);
+
+//STEP 11
+
+//let x = "Shawn Stetson";
+//let y = ", Welcome to the JavaScript class!";
+//alert ("Hello " + x  + y);
+
+//STEP 12
+
+//let name = "Shawn";
+//let y = ", Welcome to the JavaScript class!";
+//alert ("Hello " + name  + y);
+
+//STEP 13
+
+//let name = "Shawn";
+//let y = ", Welcome to the";
+//let course = " JavaScript";
+//alert ("Hello " + name  + y + course + " class!");
+
+//STEP 14
+
+//alert (`Hello Shawn Stetson. 
+//Welcome to the JavaScript class!`);
+
+//STEP 15
+
+//let name = prompt ('What is your name?');
+//let y = ", Welcome to the";
+//let course = " JavaScript";
+//alert ("Hello " + name  + y + course + " class!");
+
+//STEP 16
+
+//Replace the hardcoded string of the class you are taking with a prompt that asks the user for the class they are taking. The prompt’s response will now be captured in the course variable.
+//let name = prompt ('What is your name? Type here..');
+//let y = ", Welcome to the ";
+//let course = prompt ('What class are you in? Type here..');
+//alert ("Hello " + name  + y + course + " class!");
+
+//STEP 17
+
+//let x = 10;
+//let y = 20;
+//console.log(x+y);
+
+//STEP 18
+
+//let x = 20;
+//console.log (20 + x);
+
+//STEP 19
+
+//let x = 20;
+//console.log (5 * x);
+
+
+//STEP 20
+
+//let x = (20 % 3);
+//console.log (x);
+
+//STEP 21
+
+//let x = true;
+//let y = true;
+//console.log (x==true && y==true);
+
+//STEP 22
+
+//let myString = "five";
+//let theString = 5;
+//console.log (myString===1 || theString===1 );
